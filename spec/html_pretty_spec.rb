28a0a7252b31305020f15a8819eeff4ad4ee9f5f
fengb <benjamin.feng@gmail.com>
2012-09-26 08:45:52 -0500

    Refactored test runner.  Ruby blocks are stupid fragile...

--- spec/html_pretty_spec.rb
+++ spec/html_pretty_spec.rb
@@ -3,15 +3,19 @@ require 'html_pretty'
 require 'yaml'
 data = YAML.load_file(__FILE__.sub(/rb$/, 'yaml'))
 
-describe HtmlPretty do
-  data.each do |context_name, tests|
-    context context_name do
-      tests.each do |test_name, config|
-        it test_name do
-          output = HtmlPretty.fake_tidy(config['in'])
-          output.should == config['out'].sub(/\n$/, '')
+def parse_describe(name, hash)
+  describe name do
+    hash.each do |child_name, child_hash|
+      if child_hash.has_key?('in')
+        it child_name do
+          output = HtmlPretty.fake_tidy(child_hash['in'])
+          output.should == child_hash['out'].sub(/\n$/, '')
         end
+      else
+        parse_describe(child_name, child_hash)
       end
     end
   end
 end
+
+parse_describe(HtmlPretty, data)
