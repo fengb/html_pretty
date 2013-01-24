99aa792457bebf6d78a66372b7d35e20f07f1f6e
fengb <benjamin.feng@gmail.com>
2013-01-23 20:39:46 -0600

    Preserving comment whitespace

--- lib/html_pretty.rb
+++ lib/html_pretty.rb
@@ -5,30 +5,31 @@ module HtmlPretty
   # Self rolled because other versions have weird errors
   class << self
     def run(html, out='')
-      html = html.gsub(/^[ \t]*\n/, "")   # kill blank lines
-      html = html.gsub(/^[ \t]+/, '')     # kill opening whitespace
-      html = html.gsub(/[ \t]+$/, '')     # kill closing whitespace
-      html = html.gsub(/[ \t]+/, ' ')     # collapse all whitespace
-      html = html.gsub(/<[ \t]+/, '<')    # remove whitespace between brackets
-      html = html.gsub(/[ \t]+>/, '>')    # remove whitespace between brackets
-
       indent = 0
-      html.each_line do |line|
-        if line.start_with?('<!!!!!>')
+      html.split(/(<!--.*?-->)/).each do |blob|
+        if blob.start_with?('<!--!!!-->')
           raise
-        elsif line.start_with?('<!') || line =~ /<html.*ie/
-          out << line                     # Comments should have no alignment
+        elsif blob.start_with?('<!--') || blob =~ /<html.*ie/
+          out << blob                     # Comments should have no adjustments
         else
-          open_tags = line.scan(/<[^\/][^>]*>/).select{|d| d !~ /\/>$/}.size
-          close_tags = line.scan(/<\/[^>]*>/).size
-          if open_tags > close_tags
-            out << "  " * indent + line
-            indent += open_tags - close_tags
-            indent = 0 if indent < 0
-          else
-            indent += open_tags - close_tags
-            indent = 0 if indent < 0
-            out << "  " * indent + line
+          blob = blob.gsub(/^[ \t]*\n/, "")   # kill blank lines
+          blob = blob.gsub(/^[ \t]+/, '')     # kill opening whitespace
+          blob = blob.gsub(/[ \t]+$/, '')     # kill closing whitespace
+          blob = blob.gsub(/[ \t]+/, ' ')     # collapse all whitespace
+          blob = blob.gsub(/<[ \t]+/, '<')    # remove whitespace between brackets
+          blob = blob.gsub(/[ \t]+>/, '>')    # remove whitespace between brackets
+          blob.each_line do |line|
+            open_tags = line.scan(/<[^\/][^>]*>/).select{|d| d !~ /\/>$/}.size
+            close_tags = line.scan(/<\/[^>]*>/).size
+            if open_tags > close_tags
+              out << "  " * indent + line
+              indent += open_tags - close_tags
+              indent = 0 if indent < 0
+            else
+              indent += open_tags - close_tags
+              indent = 0 if indent < 0
+              out << "  " * indent + line
+            end
           end
         end
       end
--- spec/html_pretty_spec.rb
+++ spec/html_pretty_spec.rb
@@ -4,7 +4,7 @@ require 'html_pretty'
 describe HtmlPretty do
   it 'outputs work in progress even upon critical failure' do
     out = ''
-    expect{HtmlPretty.run("<html>\n<!!!!!>\n</html>", out)}.to raise_exception
+    expect{HtmlPretty.run("<html>\n<!--!!!-->\n</html>", out)}.to raise_exception
     out.should == "<html>\n"
   end
 end
