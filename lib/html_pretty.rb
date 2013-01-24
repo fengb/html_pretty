64e23c364e229c7d39fe3828e58d067f68e41887
fengb <benjamin.feng@gmail.com>
2013-01-23 21:12:05 -0600

    Stupid refactor

--- lib/html_pretty.rb
+++ lib/html_pretty.rb
@@ -1,41 +1,40 @@
 module HtmlPretty
   VERSION = '0.0.1'
 
+  SPECIAL = /(<!--.*?-->|<script.*?\/script>|<style.*?\/style>)/m
+
   # Really bad assumption heavy tidy...
   # Self rolled because other versions have weird errors
-  class << self
-    SPECIAL = /(<!--.*?-->|<script.*?\/script>|<style.*?\/style>)/m
-    def run(html, out='')
-      indent = 0
-      html.split(SPECIAL).each do |blob|
-        if blob.start_with?('<!--!!!-->')
-          raise
-        elsif blob =~ SPECIAL
-          out << blob << "\n"                 # Comments should have no adjustments
-        else
-          blob = blob.gsub(/^[ \t]*\n/, "")   # kill blank lines
-          blob = blob.gsub(/^[ \t]+/, '')     # kill opening whitespace
-          blob = blob.gsub(/[ \t]+$/, '')     # kill closing whitespace
-          blob = blob.gsub(/[ \t]+/, ' ')     # collapse all whitespace
-          blob = blob.gsub(/<[ \t]+/, '<')    # remove whitespace between brackets
-          blob = blob.gsub(/[ \t]+>/, '>')    # remove whitespace between brackets
-          blob.each_line do |line|
-            open_tags = line.scan(/<[^\/][^>]*>/).select{|d| d !~ /\/>$/}.size
-            close_tags = line.scan(/<\/[^>]*>/).size
-            if open_tags > close_tags
-              out << "  " * indent + line
-              indent += open_tags - close_tags
-              indent = 0 if indent < 0
-            else
-              indent += open_tags - close_tags
-              indent = 0 if indent < 0
-              out << "  " * indent + line
-            end
+  def self.run(html, out='')
+    indent = 0
+    html.split(SPECIAL).each do |blob|
+      if blob.start_with?('<!--!!!-->')
+        raise
+      elsif blob =~ SPECIAL
+        out << blob << "\n"                 # Comments should have no adjustments
+      else
+        blob = blob.gsub(/^[ \t]*\n/, "")   # kill blank lines
+        blob = blob.gsub(/^[ \t]+/, '')     # kill opening whitespace
+        blob = blob.gsub(/[ \t]+$/, '')     # kill closing whitespace
+        blob = blob.gsub(/[ \t]+/, ' ')     # collapse all whitespace
+        blob = blob.gsub(/<[ \t]+/, '<')    # remove whitespace between brackets
+        blob = blob.gsub(/[ \t]+>/, '>')    # remove whitespace between brackets
+        blob.each_line do |line|
+          open_tags = line.scan(/<[^\/][^>]*>/).select{|d| d !~ /\/>$/}.size
+          close_tags = line.scan(/<\/[^>]*>/).size
+          if open_tags > close_tags
+            out << "  " * indent + line
+            indent += open_tags - close_tags
+            indent = 0 if indent < 0
+          else
+            indent += open_tags - close_tags
+            indent = 0 if indent < 0
+            out << "  " * indent + line
           end
         end
       end
-
-      out
     end
+
+    out
   end
 end
